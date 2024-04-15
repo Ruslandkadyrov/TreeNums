c0da77adecf85d25221963b3933eb27643cf4991
Кадыров Руслан <kadyrovrr88@mail.ru>
2024-04-15 18:43:41 +0500

    Реализовано: загрузка дерева из файла hdf

--- main.py
+++ main.py
@@ -101,12 +101,14 @@ class TreeWindow(QMainWindow):
         self.button_create_tree = QPushButton("Создать дерево случайной глубины")
         self.button_fill_values = QPushButton("Заполнить пустые листья дерева случайными значениями")
         self.save_button = QPushButton('Сохранить дерево в HDF5')
+        self.download_button = QPushButton('Загрузить дерево из HDF5')
         # Доавляем кнопки в окно
         self.layout.addWidget(self.button_add)
         self.layout.addWidget(self.button_delete)
         self.layout.addWidget(self.button_create_tree)
         self.layout.addWidget(self.button_fill_values)
         self.layout.addWidget(self.save_button)
+        self.layout.addWidget(self.download_button)
         # Создание виджета QWidget для управления layout.
         self.central_widget = QWidget()
         # Установка layout для центрального виджета.
@@ -119,6 +121,7 @@ class TreeWindow(QMainWindow):
         self.button_create_tree.clicked.connect(self.create_random_tree)
         self.button_fill_values.clicked.connect(self.fill_random_values)
         self.save_button.clicked.connect(self.save_button_clicked)
+        self.download_button.clicked.connect(self.download_button_clicked)
         # График
         self.plot_widget = pg.PlotWidget()
         self.plot_widget.setMinimumWidth(250)
@@ -127,29 +130,41 @@ class TreeWindow(QMainWindow):
         self.splitter.addWidget(self.plot_widget)
         self.splitter.addWidget(self.tree_view)
 
-        # Подключаем сигнал к слоту
+        # Подключаем сигнал к обнавлению графика
         self.tree_view.nodeSignal.connect(self.update_plot)
 
+    # Обработчик создания рандномного дерева
     def create_random_tree(self, depth=0, max_depth=5, min_depth=4):
+        # Создание корневого элемента
         root_item = QStandardItem()
+        # Добавляем корневой элемент в моель
         self.model.appendRow(root_item)
+        # Запускаем метод создания дерева
         self.create_random_tree_recursive(root_item, depth, max_depth, min_depth)
 
     def create_random_tree_recursive(self, parent_item, depth, max_depth, min_depth):
+        # Если тукущая глубина больше либо равно минимальной,
+        # то рандомно назначается количесто дочерних узлоа от 1 до 4
+        # иначе назначается 4
         if depth >= min_depth:
             num_children = random.randint(1, 4)
         else:
             num_children = 4
+        # тут в каждой итерации создается дочерний узел у parent_item
         for _ in range(num_children):
             new_item = QStandardItem()
             parent_item.appendRow(new_item)
+            # проверяем текущая глубина должна быть меньше максимальной
+            # также устанвлена 50% вероятность дальнейшей рекурсии
             if depth < max_depth and random.random() < 0.50:
+                # при запуске рекурсии увеличиваем глубину на 1
                 self.create_random_tree_recursive(new_item, depth + 1, max_depth, min_depth)
 
     def fill_random_values(self):
-        current_item = self.model.invisibleRootItem()
-        self.fill_random_values_recursive(current_item)
-        self.update_values(current_item)
+        # находим коренвой елемент
+        root_item = self.model.invisibleRootItem()
+        # запускаем рекурсивную функцию заполнения значениями
+        self.fill_random_values_recursive(root_item)
 
     def fill_random_values_recursive(self, parent_item):
         if isinstance(parent_item, QStandardItem):
@@ -169,6 +184,40 @@ class TreeWindow(QMainWindow):
         tree_data = self.get_tree_data()
         file_name = 'tree_data.h5'  # Имя файла для сохранения
         self.save_tree_to_hdf(tree_data, file_name)
+
+    def download_button_clicked(self):
+        file_name = 'tree_data.h5'
+        tree_data = self.load_tree_from_hdf(file_name)
+        if tree_data:
+            self.update_tree_view(tree_data)
+            self.tree_view.nodeSignal.emit()
+
+    def load_tree_from_hdf(self, file_name):
+        tree_data = {}
+        with h5py.File(file_name, 'r') as hdf_file:
+            self.load_tree_recursive(hdf_file, tree_data)
+        return tree_data
+
+    def load_tree_recursive(self, group, data):
+        for key in group.keys():
+            if isinstance(group[key], h5py.Group):
+                data[key] = {}
+                self.load_tree_recursive(group[key], data[key])
+            elif isinstance(group[key], h5py.Dataset):
+                data[key] = group[key][()]
+
+    def update_tree_view(self, tree_data, parent_item=None):
+        if parent_item is None:
+            parent_item = self.model.invisibleRootItem()
+        for key, value in tree_data.items():
+            if isinstance(value, dict):
+                new_item = QStandardItem(key)
+                parent_item.appendRow(new_item)
+                self.checking_sign_second_node(int(key), new_item)
+                self.update_tree_view(value, new_item)
+            else:
+                new_item = QStandardItem(str(value))
+                self.checking_sign_second_node(int(value), parent_item)
 
     def save_tree_to_hdf(self, tree_data, file_name):
         with h5py.File(file_name, 'w') as hdf_file:
@@ -262,11 +311,12 @@ class TreeWindow(QMainWindow):
             self.tree_view.nodeSignal.emit()
 
     def checking_sign_second_node(self, value, node):
-        if node.parent() and node.parent().parent() is None:
-            if value > 0:
-                node.setBackground(QColor("green"))
-            elif value < 0:
-                node.setBackground(QColor("red"))
+        if node.parent():
+            if node.parent().parent() and node.parent().parent().parent() is None:
+                if value > 0:
+                    node.setBackground(QColor("green"))
+                elif value < 0:
+                    node.setBackground(QColor("red"))
 
     def calculate_avg_values(self, tree_item, level_values, level):
         if level == len(level_values):
@@ -295,7 +345,8 @@ class TreeWindow(QMainWindow):
         filtered_avg_values = [value for value in avg_values if value is not None]
         levels = list(range(len(filtered_avg_values)))
         self.plot_widget.clear()
-        self.plot_widget.plot(levels[1:],  filtered_avg_values[1:], pen=pg.mkPen('b'))
+        self.plot_widget.plot(levels,  filtered_avg_values, pen=pg.mkPen('b'))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
